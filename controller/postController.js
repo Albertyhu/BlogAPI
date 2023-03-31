425c3d9507bb2b6992f463ed75629a1bad9224ec
unknown <hualbert@rocketmail.com>
2023-03-30 19:03:00 -0700

    Categories can now retrieve their posts.

--- controller/postController.js
+++ controller/postController.js
@@ -1,5 +1,7 @@
 const Post = require('../model/post.js'); 
 const User = require('../model/user.js');
+const { ObjectId } = require('mongodb');
+
 
 exports.AllPosts = async (req, res, next) => {
     try {
@@ -69,6 +71,23 @@ exports.GetOnePostByAuthor = async (req, res, next) => {
     }
 }
 
+exports.GetPostsByCategory = async (req, res, next) => {
+    try {
+        await Post.find({ category: { $elemMatch: { $eq: new ObjectId(req.params.categoryID) } } })
+            .populate("author")
+            .populate("tag")
+            .then(result => {
+                res.status(200).json({post: result})
+            }).catch(e => {
+                console.log("There was an error retrieving posts by category: ", e)
+                res.status(500).json({ error: [{param: "server", msg: `Internal server error.`}]})
+            })
+    } catch (e) {
+        console.log("There was an error retrieving posts by category: ", e);
+        res.status(500).json({ error: [{ param: "server", msg: `Internal server error.` }] })
+    }
+}
+
 exports.DeletePostById = async (req, res, next) => {
     const PostId = req.params.id;
     try {
@@ -119,6 +138,21 @@ exports.HandleLikeToggle = async (req, res, next) => {
     }
 }
 
+//needs to be tested 
 exports.UpdateLikes = async (req, res, next) => {
-    
+    if (req.body.updatedLikes) {
+        const newUpdate = new Post({
+            likes: JSON.parse(req.body.updatedLikes),
+            _id: req.params.id,
+        })
+        await Post.findByIdAndUpdate(req.params.id, { newUpdate })
+            .then(result => {
+                console.log("Update is successful")
+            })
+            .catch(e => {
+                console.log(`There is an error in updating likes on ${req.params.id}`)
+            })
+    } else {
+        console.log(`There is a problem with the likes array passed from client`)
+    }
 }
--- route/postAPI.js
+++ route/postAPI.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const cors = require('cors'); 
 const postController = require('../controller/postController.js');
 const { upload } = require('../middleware/multerSetup.js');
-
+const { verifyToken } = require('../middleware/verifyMember.js')
 router.get('/', cors(), postController.AllPosts)
+
+router.get('/get_posts_by_category/:categoryID', cors(), postController.GetPostsByCategory);
 
 router.get('/:id', cors(), postController.FindOnePost)
 
-router.put('/:id/like', cors(), postController.UpdateLikes)
+router.put('/:id/update_likes', cors(), verifyToken, upload.none(), postController.UpdateLikes)
 
 module.exports = router; 
